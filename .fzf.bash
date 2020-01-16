e4297dccb392389237ff013bcd5d453ce124da48
ryoo14 <anana12185@gmail.com>
2020-01-16 22:18:04 +0900

    fzfのpath修正

--- .fzf.bash
+++ .fzf.bash
@@ -6,11 +6,11 @@
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$HOME/.fzf/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$HOME/.fzf/key-bindings.bash"
+source "$HOME/.fzf/shell/key-bindings.bash"
 
 # Options
 # ------------
