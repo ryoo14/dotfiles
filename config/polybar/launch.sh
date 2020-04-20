4885ef1bbc38aaede4b2832beecf8282360a5b78
ryoo14 <anana12185@gmail.com>
2020-04-20 22:19:43 +0900

    multi monitor

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,7 +1,15 @@
 #!/bin/env sh
 
-pkill polybar
+# pkill polybar
+# 
+# sleep 1;
+# 
+# polybar mybar &
 
-sleep 1;
-
-polybar mybar &
+if type "xrandr" > /dev/null 2>&1; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
