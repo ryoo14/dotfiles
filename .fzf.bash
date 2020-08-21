1aeb692142e2e3ebc12ee3d0cd9d36f420208a7d
ryoo14 <anana12185@gmail.com>
2020-08-22 00:16:03 +0900

    load key-bind

--- .fzf.bash
+++ .fzf.bash
@@ -1,19 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
+if [[ ! "$PATH" == */home/ryoo/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/ryoo/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
+# [[ $- == *i* ]] && source "/home/ryoo/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$HOME/.fzf/shell/key-bindings.bash"
-
-# Options
-# ------------
-export FZF_CTRL_T_COMMAND='find . 2> /dev/null'
-export FZF_DEFAULT_OPTS='--height 40% --reverse'
-export FZF_CTRL_R_OPTS='--sort --exact'
+source "/home/ryoo/.fzf/shell/key-bindings.bash"
